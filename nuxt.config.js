d01931679791ad1876a40ecf64ac77bc05b6ccf3
Sean Washington <hello@seanwash.com>
2017-08-21 22:55:19 -0700

    Switch to yarn, bump nuxt version

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
+  // prettier-ignore
   head: {
     titleTemplate: '%s - Sean Washington',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Full Stack Developer specializing in Elixir, Ruby on Rails, JavaScript, and CSS.' },
-      { property: 'og:description', content: 'Full Stack Developer specializing in Elixir, Ruby on Rails, JavaScript, and CSS.'},
+      { hid: 'description', name: 'description', content: 'Full Stack Developer specializing in Elixir, JavaScript, and CSS.' },
+      { property: 'og:description', content: 'Full Stack Developer specializing in Elixir, JavaScript, and CSS.'},
       { name: 'twitter:card', content: 'summary' },
       { name: 'twitter:site', content: '@seanwashbot' },
       { name: 'twitter:creator', content: '@seanwashbot' },
@@ -18,20 +16,9 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  css: [
-    'tachyons/css/tachyons.css'
-  ],
-  /*
-  ** Customize the progress-bar color
-  */
+  css: ['tachyons/css/tachyons.css'],
   loading: { color: '#000' },
-  /*
-  ** Build configuration
-  */
   build: {
-    /*
-    ** Run ESLINT on save
-    */
     extend (config, ctx) {
       if (ctx.isClient) {
         config.module.rules.push({
